a557f2fbd3cd723ca133c5552e6412f908750bed
Filip Kovacevic <f.kovacevic@gmx.at>
2022-05-12 14:39:07 +0200

    Add further tables in create.sql to include bets, betting providers, users, contracts. Add some checks on possible domain values for the position, transfer period, tip. Add comments about the source columns from the currentl three different datasets. Add README to Datasets/ do document the dataset sources and necessary transformations within the datasets.

--- create.sql
+++ create.sql
@@ -1,50 +1,95 @@
---from https://github.com/plkpiotr/soccer-league
+/*
+* source: https://github.com/plkpiotr/soccer-league
+* Modified and extended by Filip Kovacevic on 12.05.2022
+*/ 
 
-create table stadiums (
-	id_stadium smallint primary key,
-	full_name varchar(32) not null,
-	capacity integer not null
+create table stadium (
+	id_stadium smallint primary key, -- missing
+	full_name varchar(32) not null, -- missing
+	capacity integer not null -- missing
 );
 
-create table teams (
-	id_team smallint primary key,
-	id_stadium smallint references stadiums(id_stadium),
-	club varchar(25) not null,
-	shorthand varchar(3) unique not null check(length(shorthand) = 3)
+create table team (
+	id_team smallint primary key, -- auto incr
+	id_stadium smallint references stadium(id_stadium), -- missing
+	club varchar(50) not null, --df_full_premierleague.home_team, --df_full_premierleague.away_team
+	shorthand varchar(3) --unique not null check(length(shorthand) = 3)
 );
 
-create table players (
-	id_player smallint primary key,
-	id_team smallint not null references teams(id_team),
-	first_name varchar(15),
-	last_name varchar(15) not null,
-	kit smallint not null check(kit between 1 and 99),
-	position varchar(10) not null check(
-		position = 'Goalkeeper' OR position = 'Defender' OR
-		position = 'Midfielder' OR position = 'Forward'),
-	country varchar(32) not null
+create table player (
+	id_player smallint primary key, --auro incr
+	name varchar(15), --player_name
+	kit smallint, --not null check(kit between 1 and 99), --missing
+	country varchar(32) -- not null --missing
+); 
+
+create table contract (
+    id_player smallint not null references player(id_player),
+	id_team smallint not null references team(id_team),
+    transfer_period varchar(15) not null check (transfer_period = 'Summer' or transfer_period = 'Winter'), --english_premiere_league.transfer_period
+    season varchar(15) not null, --english_premiere_league.season
+	position varchar(10) not null check(position='Centre-Forward' --english_premiere_league.position
+                                        OR position='Goalkeeper'
+                                        OR position='Central Midfield'
+                                        OR position='Defensive Midfield'
+                                        OR position='Left-Back'
+                                        OR position='Right Winger'
+                                        OR position='Attacking Midfield'
+                                        OR position='Centre-Back'
+                                        OR position='Second Striker'
+                                        OR position='Right-Back'
+                                        OR position='Right Midfield'
+                                        OR position='Left Midfield'
+                                        OR position='Left Winger'
+                                        OR position='attack'),
+    year numeric(4) --english_premiere_league.year
+
 );
 
-create table matches (
-	id_match smallint primary key,
-	id_home smallint not null references teams(id_team),
-	id_away smallint not null references teams(id_team),
-	id_stadium smallint not null references stadiums(id_stadium),
-	date_time timestamp not null,
-	attendance integer not null
+create table match (
+	id_match smallint primary key, --auto incr
+	id_home smallint not null references team(id_team), --df_full_premierleague.home_team
+	id_away smallint not null references team(id_team), --df_full_premierleague.away_team
+	id_stadium smallint not null references stadium(id_stadium), --missing 
+	date_time timestamp not null, --df_full_premierleague.date
+	attendance integer not null --missing
 );
 
-create table results (
-	id_match smallint primary key references matches(id_match),
-	home smallint not null check(home >= 0),
-	away smallint not null check(away >= 0)
+create table result (
+	id_match smallint primary key references match(id_match),
+	home smallint not null check(home >= 0),--EO.FTHG
+	away smallint not null check(away >= 0) --EO.FTAG
 );
 
-create table scores (
-	id_score smallint primary key,
-	id_match smallint not null references matches(id_match),
-	id_team smallint not null references teams(id_team),
-	id_player smallint not null references players(id_player),
-	goals smallint not null check(goals >= 0),
-	assists smallint not null check(assists >= 0)
+create table score (
+	id_score smallint primary key, --auto incr
+	id_match smallint not null references match(id_match),
+	id_team smallint not null references team(id_team),
+	id_player smallint not null references player(id_player),
+	goals smallint not null check(goals >= 0) --df_full_premierleague.goal_home_ft + df_full_premierleague.goal_away_ft
+	--assists smallint not null check(assists >= 0) --missing
 );
+
+create table plattformUser (
+    id_user smallint primary key, --auto incr
+    name varchar(50) not null -- synthetic
+);
+
+create table bet (
+    id_bet smallint primary key, --auto incr
+    id_match smallint not null references match(id_match),
+    home_win_odds decimal not null, --E0.B365H		
+    draw_odds decimal not null, --E0.B365D
+    away_win_odds decimal not null, --E0.B365A
+    betting_provider varchar(50) not null -- E0.B365H column name substring
+);
+
+create table user_bet (
+    id_user smallint not null references plattformUser(id_user),
+    id_bet smallint not null references bet(id_bet),
+    tip varchar(1) not null check(tip='H' OR tip='D' OR tip ='A'), --synthetic
+    tip_timestamp timestamp not null, --synthetic
+    primary key(id_user, id_bet, tip_timestamp)
+);
+
+
