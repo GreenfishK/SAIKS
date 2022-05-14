e873ff2c3662e915f078a2b75b224a8b503e3e88
Filip Kovacevic <f.kovacevic@gmx.at>
2022-05-14 20:03:33 +0200

    load data into table team. Add load_data service to mount the data into a docker containerand execute the load_data.py script

--- scripts/import_data.py
+++ scripts/import_data.py
@@ -4,12 +4,13 @@ import sqlalchemy
 import os
 from sqlalchemy import create_engine
 
+base_dir = '/home/saiks/Datasets/'
 # Bets
-df_betting_odds = pd.read_csv('../Datasets/betting-odds/E0.csv')
+df_betting_odds = pd.read_csv(base_dir + 'betting-odds/E0.csv')
 df_betting_odds = df_betting_odds[['Date', 'Time', 'HomeTeam', 'AwayTeam', 'B365H',	'B365D', 'B365A']]
 
 # Premier League games
-df_games_premiere_league = pd.read_csv('../Datasets/games-premier-league/df_full_premierleague.csv')
+df_games_premiere_league = pd.read_csv(base_dir + 'games-premier-league/df_full_premierleague.csv')
 df_games_premiere_league = df_games_premiere_league[['date', 'home_team', 'away_team', 'result_full',
                                                      'goal_home_ft', 'goal_away_ft']]
 
@@ -17,11 +18,10 @@ df_games_premiere_league = df_games_premiere_league[['date', 'home_team', 'away_
 columns = ['club_name', 'player_name', 'position', 'club_involved_name', 'transfer_movement',
            'transfer_period', 'year', 'season']
 df_transfers = pd.DataFrame(columns=columns)
-for filename in os.listdir("../Datasets/transfers/data/2021"):
-    df = pd.read_csv("../Datasets/transfers/data/2021/" + filename)
+for filename in os.listdir(base_dir + "transfers/data/2021"):
+    df = pd.read_csv(base_dir + "transfers/data/2021/" + filename)
     df = df[columns]
     df_transfers = pd.concat([df_transfers, df], axis=0)
-
 
 # Check which teams need to be renamed
 df1 = pd.Series(pd.concat([df_betting_odds['HomeTeam'], df_betting_odds['HomeTeam']], axis=0).drop_duplicates(), name="Team1")
@@ -41,9 +41,16 @@ teams_remap = {'West Ham': 'West Ham United', 'West Brom': 'West Bromwich Albion
                'Man City': 'Manchester City'}
 df_betting_odds.replace(teams_remap, inplace=True)
 
-print(df_betting_odds)
+df_betting_odds.columns = [c.lower() for c in df_betting_odds.columns]
+df_games_premiere_league.columns = [c.lower() for c in df_games_premiere_league.columns]
+df_transfers.columns = [c.lower() for c in df_transfers.columns]
 
-# df.columns = [c.lower() for c in df.columns] # PostgreSQL doesn't like capitals or spaces
-#engine = create_engine('postgresql://username:password@localhost:5432/dbname')
-# df.to_sql("my_table_name", engine)
-#print(pd.merge(df1, df2, how='outer'))
+# Create dfs in the structure of the football db schema tables
+df_team = pd.DataFrame(columns=['id_team', 'id_stadium', 'club', 'shorthand'])
+df_team['id_team'] = np.arange(0, len(df2))
+df_team['id_stadium'] = np.nan
+df_team['club'] = df2.values
+df_team['shorthand'] = np.nan
+
+engine = create_engine('postgresql://saiks2022:saiks2022@saiks:5432/football')
+df_team.to_sql("team", engine, if_exists="append", index=False)
